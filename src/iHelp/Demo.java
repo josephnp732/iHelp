2f40621215e34996711631f83b361e6a3f721838
Suresh Kumar Reddy <sure14is@cmrit.ac.in>
2018-01-13 21:30:32 +0530

    Introducing MaryTTS
    Replaced FreeTTS by MaryTTS

---
+++ src/iHelp/Demo.java
@@ -0,0 +1,21 @@
+package iHelp;
+
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+
+class Demo
+{
+        int x,y,p,q,dx,dy;
+        
+        public Demo(int x,int y,int p,int q,int dx,int dy)
+        {
+                this.x=x;
+                this.y=y;
+                this.p=p;
+                this.q=q;
+                this.dx=dx;
+                this.dy=dy;
+        }
+}
